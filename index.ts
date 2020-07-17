08f9e544ba7d46e8427c2dd77d045126f676222a
Anwesh43 <anweshthecool0@gmail.com>
2020-07-18 01:34:54 +0530

    adding BlockStep class

--- index.ts
+++ index.ts
@@ -160,6 +160,29 @@ class BSNode {
             return curr
         }
         cb()
-        return this 
+        return this
+    }
+}
+
+class BlockStep {
+
+    curr : BSNode = new BSNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
     }
 }
