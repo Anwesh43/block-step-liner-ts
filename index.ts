60d9de61216ddf8c6c745d7770ae90548738564e
Anwesh43 <anweshthecool0@gmail.com>
2020-07-18 00:51:13 +0530

    adding constants

---
+++ index.ts
@@ -0,0 +1,6 @@
+const blocks : number = 5
+const scGap : number = 0.02 / blocks
+const w : number = window.innerWidth
+const h : number = window.innerHeight
+const backColor : string = "#BDBDBD"
+const colors : Array<string> = ["#009688", "#2196F3", "#f44336", "#4CAF50", "#3F51B5"]
